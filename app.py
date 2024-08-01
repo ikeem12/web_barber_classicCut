a7e2b145338559e3ccd0aebb399ed28de0dbf4fa
johan sebastian <baloncestonba222@gmail.com>
2024-08-01 09:44:11 +0200

    add the path to the login and create the login with its styles

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask import render_template,redirect,Response
 from flask_mysqldb import MySQL,MySQLdb
 
-app = Flask(__name__, static_folder='src/style.css', template_folder='src/template')  
+app = Flask(__name__, static_folder='src/static', template_folder='src/template')  
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/admin')
+def login():
+    return render_template('auth/login.html')
 
 if __name__ == '__main__':
     app.run()
